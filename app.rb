f616302f9ba0efc24b74ab6b85df731a54c4b466
Nakai <nakai.hamlin18@gmail.com>
2021-04-07 02:29:28 -0400

    commit tree 5cc3899a9770984fd0a107b3923e890cbb221436

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Nakai."
+      end
+    
+      get '/hometown' do
+        "My hometown is Connecticut."
+      end
+    
+      get '/favorite-song' do
+        "My favorite song is Dreamy Night."
+      end
+    
 end
